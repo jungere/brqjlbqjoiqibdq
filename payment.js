a67f4bd07c4d7546ab584075e3d1432840b860aa
dico.zhang <345960918@qq.com>
2016-06-06 22:39:42 +0800

    payment API submit

--- payment.js
+++ payment.js
@@ -1,65 +1,118 @@
+var BaseController = require("./Base");
 var request = require('request');
-var Config = require('../global/config.js')
-var Util = require('../global/util.js');
-var APPKEY = Config.KEY;
-var SECRET = Config.SECRET;
+var _ = require("underscore")
 
-var UNOPayURL = 'http://demo.unochat.io:8080/Server';
-
-
-module.exports = {
+module.exports = BaseController.extend({
+    URL : {
+        API :  "",
+        Recharge :  "",
+        PaySubmit : "",
+        PaySubmitWithoutCode : ""
+    },
     
-    /*
-     * Receive and Send Tokens as a Unochat Merchant Account 
-     * unochat_uid   User ID of unochat
-     * orderNo   Your preferred Order No.
-     * amout Amount to be transacted 
-     * orderTime   Transaction Time 
-     * remark    Remarks
-     * return mixed     Status Description
+    init : function(params){
+        this.URL = _.extend(this.URL, params);
+        return this;
+    },
+    
+    /**
+     * return mixed Status Description
      * 101 Authorization Failed
      * 102 Failed to retrieve Order No
      * 103 Invalid Amount
      * 104 Failed to initiate Transaction
      * 105 Invalid Unochat ID, Payment suspended for 10 minutes
      * 106 Insufficient Fund in Merchant's account 
+     * 107-动态口令格式不正确
+     * 108-动态口令错误次数过多 10分钟冻结时间
      * 109 Invalid Authentication Code
      * 110 Unable to Deduct Amount
      */
-     
-     
-     /* Collect Tokens from Unochat users to my Unochat Merchant Account */
-     
-    unoRecharge : function(unochatId, orderNo, amount, orderTime, remark, callback){
+    
+    
+    /**
+     * 麦信支付
+     * @param $unochatUid User ID of unochat
+     * @param $unochatAccount Your preferred Order No.
+     * @param $dynamicCode dynamicc ode
+     * @param $orderNo Your preferred Order No.
+     * @param $amount Amount to be transacted
+     * @param $orderTime Transaction Time
+     * @param $remark Remarks
+     */
+    
+    unoPaySubmit : function(unochatUid, unochatAccount, dynamicCode, orderNo, amount, orderTime, remark){
         var params = {
-            unochat_uid : unochatId,
+            unochat_uid : unochatUid,
+            unochat_account : unochatAccount,
+            dynamic_code : dynamicCode,
+            order_no : orderNo,
+            amount : amount,
+            order_time : orderTime,
+            remark : remark       
+        }; 
+        var url = this.URL.PaySubmit;
+        var req = this.createPayRequest(url, params);
+        // console.log(req);
+        request(req, function(err, response, body){
+            console.log(body);
+            
+        })
+    },
+    
+    /**
+     * 麦信支付，无需动态口令
+     * @param $unochatUid User ID of unochat
+     * @param $unochatAccount Your preferred Order No.
+     * @param $dynamicCode dynamicc ode
+     * @param $orderNo Your preferred Order No.
+     * @param $amount Amount to be transacted
+     * @param $orderTime Transaction Time
+     * @param $remark Remarks
+     */
+    unoPaySubmitWithoutCode : function(unochatUid,  orderNo, amount, orderTime, remark){
+        var params = {
+            unochat_uid : unochatUid,
+            order_no : orderNo,
+            amount : amount,
+            order_time : orderTime,
+            remark : remark       
+        }; 
+        var url = this.URL.PaySubmitWithoutCode;
+        var req = this.createPayRequest(url, params);
+        console.log(req);
+        request(req, function(err, response, body){
+            
+            console.log(err, body);
+            
+        })
+    },
+    
+    /*
+     * 麦信积分充值
+     * unochat_uid User ID of unochat
+     * orderNo Your preferred Order No.
+     * amout Amount to be transacted 
+     * orderTime Transaction Time
+     * remark Remarks
+     */
+    unoRecharge : function(unochatUid, orderNo, amount, orderTime, remark, callback){
+        var params = {
+            unochat_uid : unochatUid,
             order_no : orderNo,
             amount : amount,
             order_time : orderTime,
             remark : remark       
         };
-        var url = UNOPayURL+ "/Pay/recharge";
-        var auth = Util.createRequestHeader(APPKEY, SECRET);
-        
-        request({
-            headers: {
-                'content-type' : 'application/json',
-                'Authorization' : 'Basic '+auth,
-            },
-            uri: url,
-            body: Util.json2string(params) ,
-            method: 'POST' 
-        }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body);
-                callback && callback(body);
-            }
+        var url = this.URL.Recharge;
+        var req = this.createPayRequest(url, params);
+        // console.log(req);
+        request(req, function(err, response, body){
+            console.log(body);
         })
+        
     },
-    
-    /* Send tokens from My unochat Merchant account to a designated Unochat Users */
-    
     unoWithdraw : function(){
         
     }
-}
+})
